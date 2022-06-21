7e6dbf4f7969a5e8a778f885ef7bee9d0616a0f3
Kundan <kundan kamble>
2022-06-21 13:07:56 -0700

    [KUNDAN] Added Create product review  program

---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Product_Review_Management
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Product Review Management");
+
+            ProductReviewManagement productReview = new ProductReviewManagement();
+            productReview.AddProductReview();
+            productReview.DisplayList();
+        }
+    }
+}
+
