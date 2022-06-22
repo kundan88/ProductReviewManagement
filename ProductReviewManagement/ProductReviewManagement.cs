d2590b8bff6ddc39546413d68b125b1b33cc2e8d
Kundan <kundan kamble>
2022-06-21 22:10:27 -0700

    [KUNDAN] Added Greater Than 3 records product review  program

---
+++ ProductReviewManagement/ProductReviewManagement.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_Review_Management
+{
+    public class ProductReviewManagement
+    {
+        public List<ProductReview> ProductReviewsList = new List<ProductReview>();
+        DataTable productdt;
+
+        public int AddProductReview()
+        {
+            ProductReviewsList.Add(new ProductReview() { ProductID = 1, UserID = 1, Rating = 1, Review = "good", IsLike = true });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 1, UserID = 1, Rating = 1, Review = "good", IsLike = true });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 3, UserID = 2, Rating = 1, Review = "good", IsLike = true });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 4, UserID = 2, Rating = 2, Review = "bad", IsLike = false });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 5, UserID = 4, Rating = 2, Review = "bad", IsLike = true });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 5, UserID = 3, Rating = 2, Review = "good", IsLike = false });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 7, UserID = 4, Rating = 3, Review = "bad", IsLike = true });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 7, UserID = 4, Rating = 3, Review = "good", IsLike = true });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 9, UserID = 5, Rating = 3, Review = "bad", IsLike = false });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 10, UserID = 5, Rating = 4, Review = "good", IsLike = false });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 10, UserID = 6, Rating = 4, Review = "nice", IsLike = true });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 10, UserID = 6, Rating = 4, Review = "bad", IsLike = true });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 10, UserID = 7, Rating = 5, Review = "good", IsLike = false });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 1, UserID = 7, Rating = 5, Review = "nice", IsLike = false });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 4, UserID = 8, Rating = 5, Review = "good", IsLike = false });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 16, UserID = 8, Rating = 1, Review = "good", IsLike = true });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 18, UserID = 9, Rating = 1, Review = "good", IsLike = true });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 18, UserID = 9, Rating = 1, Review = "very bad", IsLike = false });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 19, UserID = 10, Rating = 2, Review = "bad", IsLike = true });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 20, UserID = 10, Rating = 3, Review = "good", IsLike = false });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 21, UserID = 11, Rating = 2, Review = "average", IsLike = true });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 9, UserID = 11, Rating = 5, Review = "bad", IsLike = false });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 25, UserID = 12, Rating = 3, Review = "good", IsLike = true });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 25, UserID = 12, Rating = 3, Review = "average", IsLike = true });
+            ProductReviewsList.Add(new ProductReview() { ProductID = 24, UserID = 12, Rating = 4, Review = "good", IsLike = true });
+            return ProductReviewsList.Count;
+        }
+       
+        
+        public string RetrieveRecords()
+        {
+            AddProductReview();
+            string nameList = "";
+            var productList = (from product in ProductReviewsList where product.Rating > 3 && (product.ProductID == 1 || product.ProductID == 4 || product.ProductID == 9) select product);
+            foreach (var product in productList)
+            {
+                nameList += product.UserID + " ";
+                Console.WriteLine("ProductId: {0} : UserId: {1} : Review: {2} : Rating: {3} : IsLike:{4}\n", product.ProductID, product.UserID, product.Review, product.Rating, product.IsLike);
+            }
+            return nameList;
+        }
+    }
+}
+
---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Product_Review_Management
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Product Review Management");
+
+            ProductReviewManagement productReview = new ProductReviewManagement();
+            productReview.RetrieveRecords();        }
+    }
+}
+
